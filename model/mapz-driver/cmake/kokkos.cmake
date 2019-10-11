bd2c005f40b436b0543a176190f68725bf360732
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-10-11 11:20:06 -0400

    KOKKOS_SRC_DIR is now a CMake variable instead of an environment variable

--- model/mapz-driver/cmake/kokkos.cmake
+++ model/mapz-driver/cmake/kokkos.cmake
@@ -1,8 +1,8 @@
 # Kokkos
-if(NOT DEFINED ENV{KOKKOS_SRC_DIR})
-  message(FATAL_ERROR "ERROR: env var KOKKOS_SRC_DIR is not defined")
-endif()
+if(NOT EXISTS ${KOKKOS_SRC_DIR})
+  message(FATAL_ERROR "ERROR: Must specify KOKKOS_SRC_DIR with cmake ... -DKOKKOS_SRC_DIR=<path>.")
+endif ()
 
-add_subdirectory($ENV{KOKKOS_SRC_DIR} ${CMAKE_CURRENT_BINARY_DIR}/kokkos)
+add_subdirectory(${KOKKOS_SRC_DIR} ${CMAKE_BINARY_DIR}/kokkos)
 
 include_directories(${Kokkos_INCLUDE_DIRS_RET})
