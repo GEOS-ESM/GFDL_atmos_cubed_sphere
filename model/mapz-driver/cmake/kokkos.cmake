918724c5c92ddf79f181df1a4922d81642567408
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2019-10-11 12:59:18 -0400

    Renamed CMake variable KOKKOS_SRC_DIR to KOKKOS_PATH

--- model/mapz-driver/cmake/kokkos.cmake
+++ model/mapz-driver/cmake/kokkos.cmake
@@ -1,8 +1,8 @@
 # Kokkos
-if(NOT EXISTS ${KOKKOS_SRC_DIR})
-  message(FATAL_ERROR "ERROR: Must specify KOKKOS_SRC_DIR with cmake ... -DKOKKOS_SRC_DIR=<path>.")
+if(NOT EXISTS ${KOKKOS_PATH})
+  message(FATAL_ERROR "ERROR: Must specify KOKKOS_PATH with cmake ... -DKOKKOS_PATH=<path>.")
 endif ()
 
-add_subdirectory(${KOKKOS_SRC_DIR} ${CMAKE_BINARY_DIR}/kokkos)
+add_subdirectory(${KOKKOS_PATH} ${CMAKE_BINARY_DIR}/kokkos)
 
 include_directories(${Kokkos_INCLUDE_DIRS_RET})
