fa0e673f9671193e88890c44094906ed1ea3dcad
William Putman <William.M.Putman@nasa.gov>
2021-02-25 16:19:15 -0500

    Updated FV3, GFDL and cloudnew ice_fractions to use MODIS polynomial as in GF

--- model/fv_cmp.F90
+++ model/fv_cmp.F90
@@ -146,7 +146,7 @@ subroutine fv_sat_adj (mdt, zvir, is, ie, js, je, ng, hydrostatic, consv_te, &
     real, dimension (is:ie) :: mc_air, lhl, lhi
     
     real :: qsw, rh
-    real :: tc, qsi, dqsdt, dq, dq0, pidep, qi_crt, tmp, dtmp
+    real :: tc, qsi, dqsdt, dq, dq0, pidep, qi_crt, tmp, dtmp, ptc
     real :: tin, rqi, q_plus, q_minus
     real :: sdt, dt_bigg, adj_fac
     real :: fac_smlt, fac_r2g, fac_i2s, fac_imlt, fac_l2r, fac_v2l, fac_l2v, fac_frz
@@ -714,7 +714,7 @@ subroutine fv_sat_adj (mdt, zvir, is, ie, js, je, ng, hydrostatic, consv_te, &
             
             do i = is, ie
                 
-                tin = pt1 (i) - (lcp2 (i) * q_cond (i) + icp2 (i) * q_sol (i)) ! minimum temperature
+                tin = pt1 (i) - (lcp2 (i) * q_liq (i) + icp2 (i) * q_sol (i)) ! minimum temperature
                 ! tin = pt1 (i) - ((lv00 + d0_vap * pt1 (i)) * q_cond (i) + &
                 ! (li00 + dc_ice * pt1 (i)) * q_sol (i)) / (mc_air (i) + qpz (i) * c_vap)
                 
@@ -735,8 +735,12 @@ subroutine fv_sat_adj (mdt, zvir, is, ie, js, je, ng, hydrostatic, consv_te, &
                     if (q_cond (i) > 1.e-6) then
                         rqi = q_sol (i) / q_cond (i)
                     else
-                        ! mostly liquid water clouds at initial cloud development stage
-                        rqi = ((tice - tin) / (tice - t_wfr))
+                       !! mostly liquid water clouds at initial cloud development stage
+                       !rqi = ((tice - tin) / (tice - t_wfr))
+                      ! Use MODIS polynomial from Hu et al, DOI: (10.1029/2009JD012384) 
+                        tc = tin-tice ! convert to celcius
+                        ptc = 7.6725 + 1.0118*tc + 0.1422*tc**2 + 0.0106*tc**3 + 0.000339*tc**4 + 0.00000395*tc**5
+                        rqi = 1.0 - (1.0/(1.0 + exp(-1*ptc))) 
                     endif
                     qstar (i) = rqi * qsi + (1. - rqi) * qsw
                 endif
