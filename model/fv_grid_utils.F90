c7573f7e9a5cbf82d4d252cdb48ef4b42a3b13db
William Putman <William.M.Putman@nasa.gov>
2024-02-21 11:43:58 -0500

    updated schmidt transform code to correct precision errors on certain grids

--- model/fv_grid_utils.F90
+++ model/fv_grid_utils.F90
@@ -921,9 +921,11 @@
     real(kind=R_GRID), intent(inout), dimension(i1:i2,j1:j2):: lon, lat
 !
     real(f_p):: lat_t, sin_p, cos_p, sin_lat, cos_lat, sin_o, p2, two_pi
-    real(f_p):: c2p1, c2m1
+    real(f_p):: c2p1, c2m1, one, near_zero, f_p_lon, f_p_lat
     integer:: i, j
 
+    one = 1.d0
+    near_zero = tiny(near_zero)
     p2 = 0.5d0*pi
     two_pi = 2.d0*pi
 
@@ -939,28 +941,32 @@
 
     do j=j1,j2
        do i=i1,i2
-          if ( abs(c2m1) > 1.d-7 ) then
-               sin_lat = sin(lat(i,j)) 
+          f_p_lon = lon(i,j)
+          f_p_lat = lat(i,j)
+          if ( abs(c2m1) > near_zero ) then
+               sin_lat = sin(f_p_lat) 
                lat_t = asin( (c2m1+c2p1*sin_lat)/(c2p1+c2m1*sin_lat) )
           else         ! no stretching
-               lat_t = lat(i,j)
+               lat_t = f_p_lat
           endif
           sin_lat = sin(lat_t) 
           cos_lat = cos(lat_t) 
-            sin_o = -(sin_p*sin_lat + cos_p*cos_lat*cos(lon(i,j)))
-          if ( (1.-abs(sin_o)) < 1.d-7 ) then    ! poles
-               lon(i,j) = 0.d0
-               lat(i,j) = sign( p2, sin_o )
+            sin_o = -(sin_p*sin_lat + cos_p*cos_lat*cos(f_p_lon))
+          if ( (one-abs(sin_o)) < near_zero ) then    ! poles
+               f_p_lon = 0.d0
+               f_p_lat = sign( p2, sin_o )
           else
-               lat(i,j) = asin( sin_o )
-               lon(i,j) = lon_p + atan2( -cos_lat*sin(lon(i,j)),   &
-                          -sin_lat*cos_p+cos_lat*sin_p*cos(lon(i,j)))
-               if ( lon(i,j) < 0.d0 ) then
-                    lon(i,j) = lon(i,j) + two_pi
-               elseif( lon(i,j) >= two_pi ) then
-                    lon(i,j) = lon(i,j) - two_pi
+               f_p_lat = asin( sin_o )
+               f_p_lon = lon_p + atan2( -cos_lat*sin(f_p_lon),   &
+                          -sin_lat*cos_p+cos_lat*sin_p*cos(f_p_lon))
+               if ( f_p_lon < 0.d0 ) then
+                    f_p_lon = f_p_lon + two_pi
+               elseif( f_p_lon >= two_pi ) then
+                    f_p_lon = f_p_lon - two_pi
                endif
           endif
+          lon(i,j) = f_p_lon
+          lat(i,j) = f_p_lat
        enddo
     enddo
 
