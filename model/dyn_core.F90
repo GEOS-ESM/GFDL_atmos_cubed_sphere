f8a48d83a67356e345c1e80f7a18ea6587215ee2
Florian Deconinck <deconinck.florian@gmail.com>
2024-06-11 09:55:05 -0400

    FV_Dynamics savepoints

--- model/dyn_core.F90
+++ model/dyn_core.F90
@@ -29,12 +29,12 @@ module dyn_core_mod
 
 #ifdef SERIALIZE
 USE m_serialize, ONLY: &
-  fs_write_field, &
+  fs_disable_serialization, &
   fs_read_field, &
-  fs_add_savepoint_metainfo, &
+  fs_write_field, &
   fs_create_savepoint, &
   fs_enable_serialization, &
-  fs_disable_serialization
+  fs_add_savepoint_metainfo
 USE utils_ppser, ONLY:  &
   ppser_get_mode, &
   ppser_savepoint, &
