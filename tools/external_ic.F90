8d27120399be4d13c293559c99b42a1710d667dd
Tom Clune <thomas.l.clune@nasa.gov>
2026-04-02 10:49:10 -0400

    Replace 'use MAPL' with 'use MAPL2' and add MAPL2 CMake dependency in fvdycore

--- tools/external_ic.F90
+++ tools/external_ic.F90
@@ -18,7 +18,7 @@ module external_ic_mod
 
    use constants_mod,     only: pi=>pi_8, omega, grav, kappa, rdgas, rvgas, cp_air
 #ifdef MAPL_MODE
-   use MAPL
+   use MAPL2
 #endif
    use, intrinsic :: iso_fortran_env, only: REAL64, REAL32
 
